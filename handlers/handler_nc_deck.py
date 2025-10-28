235d418adc6e14f9b11a36c956b1b75e30076075
Щербинин Андрей Владимирович <scherbininav@it.mos.ru>
2025-10-28 11:19:51 +0300

    feat(NC): Добавлено получение стеков из доски
    #12

--- handlers/handler_nc_deck.py
+++ handlers/handler_nc_deck.py
@@ -1,4 +1,4 @@
-"""Обработчик получения досок"""
+"""Обработчик получения досок и данных в них"""
 
 from typing import List, Optional
 
@@ -8,11 +8,12 @@ from config.settings import settings
 from handlers.handler_logging import logger
 from handlers.handler_requests import send_request
 from models.model_board import ModelBoard
+from models.model_stack import ModelStack
 
 DECK_ENDPOINT = "/apps/deck/api/v1.0"
 
 
-async def get_decks() -> Optional[List[ModelBoard]]:
+async def get_boards() -> Optional[List[ModelBoard]]:
     """
     Получение всех Дэк
 
@@ -28,5 +29,30 @@ async def get_decks() -> Optional[List[ModelBoard]]:
         except ValidationError as err:
             logger.critical(f"Получены невалидные данные: {err}")
     else:
-        logger.critical("Данные не получены")
+        logger.critical("Данные по доскам не получены")
+    return None
+
+
+async def get_stacks(board_id: int) -> Optional[List[ModelStack]]:
+    """
+    Получение стеков в доске
+
+    :param board_id: ID доски
+    :type board_id: int
+    :return: Список стеков в доске
+    :rtype: List[ModelStack] | None
+    """
+    if board_id is not None:
+        stacks_endpoint = f"{settings.NC_URL}{DECK_ENDPOINT}/{board_id}/stacks"
+        stacklist = await send_request(url=stacks_endpoint, method="GET")
+        if stacklist is not None:
+            try:
+                stacks = [ModelStack(**stack) for stack in stacklist]
+                return stacks
+            except ValidationError as err:
+                logger.critical(f"Получены невалидные данные: {err}")
+        else:
+            logger.critical("Данные по стекам не получены")
+    else:
+        logger.critical('"board_id" не задан')
     return None
