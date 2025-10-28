194c5047d6a163d8dbb71e8f388cb338e731cbd3
intervisionlord <ads-net@mail.ru>
2025-10-28 03:07:04 +0300

    refactor(Bot): Логика запросов через отдельный хендлер
    - Запросы через handler_requests
    - Улучшено ответное сообщение со списком
    - Добавлены докстринги
    
    #12 #15

--- handlers/bot_routes/nc_deck_routes.py
+++ handlers/bot_routes/nc_deck_routes.py
@@ -1,18 +1,32 @@
+"""Обработчик отображения доступных досок"""
+
 from aiogram import Router, types
 from aiogram.filters import Command
 
 from handlers.handler_logging import logger
-from handlers.handler_nc_deck import get_decks_in_workspace
-from models.model_board import ModelBoard
+from handlers.handler_nc_deck import get_decks
 
 nc_deck_router = Router()
 
 
 @nc_deck_router.message(Command("decks"))
-async def get_decks(message: types.Message):
-    logger.info(f"Запрошен список карточек пользователем {message.from_user.id}")
-    decks_json = await get_decks_in_workspace()
-    decks = [ModelBoard.model_validate(deck) for deck in decks_json]
-    await message.answer(
-        f"Доступные списки: {', '.join([deck.title for deck in decks])}"  # TODO: Дебаг
+async def command_get_decks(message: types.Message) -> None:
+    """
+    Отправляет в ответ список доступных досок
+
+    :param message: Ответное сообщение
+    :type message: types.Message
+    """
+    logger.info(
+        f"Запрошен список досок пользователем {message.from_user.id} "
+        f"({message.from_user.username})"
     )
+    decks = await get_decks()
+    if decks is not None:
+        await message.answer(
+            "<b>🗃️ Доступные доски:</b>\n\n"
+            f"{'\n'.join([f'    🗂️ {deck.title}' for deck in decks])}",
+            parse_mode="HTML",
+        )
+    else:
+        await message.answer("❌ Доски отсутствуют или не получены")
